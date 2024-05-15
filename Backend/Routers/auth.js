be53e77f06537565df066a52101f7fe8836cf946
Pedro Lopez <plopez-2019280@kinal.edu.gt>
2024-05-14 22:05:36 -0600

    uploading backend files

---
+++ Backend/Routers/auth.js
@@ -0,0 +1,17 @@
+const express = require("express")
+
+const {register,login, getPrivateData} = require("../Controllers/auth");
+
+const { getAccessToRoute } = require("../Middlewares/Authorization/auth");
+
+const router = express.Router() ;
+
+
+router.post("/register",register)
+
+router.post("/login",login)
+
+router.get("/private",getAccessToRoute,getPrivateData)
+
+
+module.exports = router
